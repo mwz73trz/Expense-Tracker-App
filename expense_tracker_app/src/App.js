65b383c6f5f4c0849064543495f8bff39b421f70
mwz73trz <michaelzura@bellsouth.net>
2022-01-10 08:29:55 -0500

    initial commit

---
+++ expense_tracker_app/src/App.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import ExpensesFilter from "./components/Expenses/ExpensesFilter";
+import ExpensesList from "./components/Expenses/ExpensesList";
+import NewExpense from "./components/NewExpense/NewExpense";
+import ExpensesChart from "./components/Expenses/ExpensesChart";
+
+const DummyExpense = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+const App = () => {
+  const [expenses, setExpenses] = useState(DummyExpense);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredItems = expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      <ExpensesChart expenses={filteredItems} />
+      <ExpensesList items={filteredItems} />
+    </div>
+  );
+};
+
+export default App;
---
+++ expense_tracker_app/src/components/Chart/Chart.js
@@ -0,0 +1,20 @@
+import ChartBar from "./ChartBar";
+import "./Chart.css";
+const Chart = (props) => {
+  const dataPointsValues = props.datapoints.map((datapoint) => datapoint.value);
+  const totalMaximum = Math.max(...dataPointsValues);
+  return (
+    <div className="chart">
+      {props.datapoints.map((datapoint) => (
+        <ChartBar
+          value={datapoint.value}
+          maxValue={totalMaximum}
+          label={datapoint.label}
+          key={datapoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
